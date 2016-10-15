bbac173a82a7caba07363266f259b91cf5f03535
DESKTOP-M9LV3CL\筒隠　有子 <h.yoshisuke@gmail.com>
2016-10-15 23:03:05 +0900

    no message

---
+++ Assets/script/SceneLoadScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneLoadScript : MonoBehaviour {
+	public GameObject botanname;
+	public void SceneLoad (){
+
+		if(botanname.name=="Stert"){
+			Application.LoadLevel ("main");
+		}
+		if(botanname.name=="restert"){
+			Application.LoadLevel ("main");
+		}
+		if(botanname.name=="gotostert"){
+			Application.LoadLevel ("Stert");
+		}
+
+	}
+
+}
